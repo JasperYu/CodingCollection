f16376751c5d11d7b532c10252fd5037deb7ec8c
Yu <jpyu@mail.ustc.edu.cn>
2017-09-10 06:40:58 +0800

    first commit test

--- src/main/java/BFS_Graph.java
+++ src/main/java/BFS_Graph.java
@@ -2,4 +2,5 @@
  * Created by Yu on 2017/9/9.
  */
 public class BFS_Graph {
+    // 分别用邻接表和邻接矩阵实现BFS
 }
--- src/main/java/DFS_Graph.java
+++ src/main/java/DFS_Graph.java
@@ -2,4 +2,5 @@
  * Created by Yu on 2017/9/9.
  */
 public class DFS_Graph {
+    // 分别用邻接表和邻接矩阵实现DFS
 }
